db8eb3aa749ddb965cac9a59cf218489d0298235
Aniket7754 <aniketkesharwani470@gmail.com>
2025-05-08 22:53:53 -0700

    New

---
+++ 1st.js
@@ -0,0 +1,26 @@
+// second largest element
+
+
+function findSecondLargest(arr) {
+    if (arr.length < 2) return null;
+  
+    let largest = -Infinity;
+    let secondLargest = -Infinity;
+  
+    for (let num of arr) {
+      if (num > largest) {
+        secondLargest = largest;
+        largest = num;
+      } else if (num > secondLargest && num < largest) {
+        secondLargest = num;
+      }
+    }
+  
+    return secondLargest === -Infinity ? null : secondLargest;
+  }
+  
+  // Example usage:
+  const arr = [10, 20, 4, 45, 99, 99];
+  const result = findSecondLargest(arr);
+  console.log("Second largest element is:", result);
+  
